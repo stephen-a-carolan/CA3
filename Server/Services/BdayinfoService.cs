bccf301cc5bf47b5235866fc014f8716bb0c1321
Stephen Carolan <X00145825@mytudublin.ie>
2022-12-12 02:05:27 +0000

    Add project files.

---
+++ Server/Services/BdayinfoService.cs
@@ -0,0 +1,15 @@
+﻿namespace CA3.Server.Services
+{
+    public class BdayinfoService : iBdayinfo
+    {
+        private readonly HttpClient httpClient;
+        public BdayinfoService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public Task<iBdayinfo> GetBdaysinfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Server/Services/iBdayinfo.cs
@@ -0,0 +1,9 @@
+﻿using CA3.Client.Pages;
+
+namespace CA3.Server.Services
+{
+    public interface iBdayinfo
+    {
+        Task<iBdayinfo<BdayInfo>>GetBdaysinfo();
+    }
+}
